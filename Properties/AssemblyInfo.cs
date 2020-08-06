3839fbedfb45637f58e039812717f6ca3cbcee3a
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-06 19:19:10 -0300

    testando develop

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,6 +31,7 @@ using System.Runtime.InteropServices;
 //
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-snap.1+Branch.develop.Sha.bf5f07fc1d6b9c53477e73ecddc29f3f0e68e65b")]
