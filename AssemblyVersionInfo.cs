a3b15546fb297d1194352858b7e72e0f469b2d21
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-07 16:04:59 -0300

    test script

---
+++ AssemblyVersionInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("1.5.0-snap.18+Branch.develop.Sha.71a5ef7065a973b2afda37a0f1e30a813df16dcb")]
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-snap.18+Branch.develop.Sha.71a5ef7065a973b2afda37a0f1e30a813df16dcb")]
+//[assembly: AssemblyVersion("1.5.0.0")]
+//[assembly: AssemblyFileVersion("1.5.0.0")]
+//[assembly: AssemblyInformationalVersion("1.5.0-snap.18+Branch.develop.Sha.71a5ef7065a973b2afda37a0f1e30a813df16dcb")]
 
