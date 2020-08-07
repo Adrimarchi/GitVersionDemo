dd76dc1b5ae44c041629e623ff1f1cfa530d8463
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-07 16:00:24 -0300

    definir versao

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-snap.16+Branch.develop.Sha.39e71f924aa282c3b1f0e42f32be9069c0893739")]
+[assembly: AssemblyInformationalVersion("1.5.0-snap.18+Branch.develop.Sha.71a5ef7065a973b2afda37a0f1e30a813df16dcb")]
 
