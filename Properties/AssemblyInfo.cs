4cb37dab62405c899c7b7de266ec6bd17ecfa09a
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-07 11:10:03 -0300

    definir versao

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,14 +31,8 @@ using System.Runtime.InteropServices;
 //
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
-<<<<<<< HEAD
-// [assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.1-ci.1+Branch.master.Sha.798aa36d55cf3ad1020fdfc5013e6cc2721d19a2")]
-=======
-// [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-rc.0+Branch.release-1.2.0.Sha.9e3a3fd810bc820a7c61d62aa55c7a3fe0c255ae")]
->>>>>>> release/1.2.0
+// [assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0-snap.1+Branch.develop.Sha.ed15edf0fbb70770ee109bdcef99163f29e37135")]
+
