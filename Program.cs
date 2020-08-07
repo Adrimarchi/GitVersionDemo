d1ba6cd6047bd23b4c19cc6371df4d44963724f0
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-07 11:45:20 -0300

    release ok

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace GitVersionDemo
         static void Main(string[] args)
         {
             Console.WriteLine("test04");
-            Console.WriteLine("releaseTeste4");
+            Console.WriteLine("releaseTeste04");
         }
     }
 }
