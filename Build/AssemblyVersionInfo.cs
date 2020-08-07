8c597b19c4826635a1c2e860d1274b3f54ac64fb
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-07 16:15:24 -0300

    definir versao

--- Build/AssemblyVersionInfo.cs
+++ Build/AssemblyVersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-snap.20+Branch.develop.Sha.a3b15546fb297d1194352858b7e72e0f469b2d21")]
+[assembly: AssemblyInformationalVersion("1.5.0-snap.26+Branch.develop.Sha.b52db6a0db8e15d50dd548f4b27fecb583a7ebd6")]
