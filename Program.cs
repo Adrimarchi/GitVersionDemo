8952d0bb02f3609d390a161b8992a3378ebfe52a
Daniel <daniel.marchi@portoseguro.com.br>
2020-08-11 18:33:57 -0300

    teste de tag develop

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace GitVersionDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("qualquer coisa3");
+            Console.WriteLine("qualquer coisa4");
             Console.WriteLine("releaseTeste08");
         }
     }
